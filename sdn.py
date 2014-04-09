e80b9cda9a20302b3f62bdf9b194a11fdf3298a2
Nick Matthews <matthnick@gmail.com>
2014-04-08 20:10:56 -0700

    changed to env variables

--- sdn.py
+++ sdn.py
@@ -3,11 +3,12 @@ import re
 from bs4 import BeautifulSoup
 import json
 import smtplib
+import os
 
 def send_email(jobs):
-    sender = 'nicmatth-test@cisco.com'
+    sender = os.environ['EMAIL_ADDRESS']
     receivers = 'nicmatth@cisco.com'
-
+    server = smtplib.SMTP(os.environ['EMAIL_SERVER']+":587")
     message = """From: nicmatth-test@cisco.com
 To:nicmatth@cisco.com
 Subject: New jobs found
@@ -16,11 +17,12 @@ This is a job alert from nicmatth-script-v0.1.0.
     """
     for job in jobs:
         message += '\n' + job['title'] + '\n' + job['link'] +'\n'
+    server.starttls()
+    server.login(os.environ['EMAIL_ADDRESS'],os.environ['EMAIL_PASSWORD'])
     try:
-        smtpObj = smtplib.SMTP( "outbound.cisco.com" , 25 )
-        smtpObj.sendmail(sender, receivers, message)  
+        server.sendmail(sender, receivers, message)  
     except:
-        smtpObj.sendmail(sender, receivers, "Data failed")
+        server.sendmail(sender, receivers, "\n\nData Failed")
 
 longurl = "https://www2.apply2jobs.com/RSS/index.cfm?fuseaction=RSSSearch&UUID=AD5852FF-BDB9-44EE-78C577568756A0B6&Lang=en&Params=2$0a74ZWOTmJIelkcLP-FaFUotawlrdM0U58bCQgIkH9xVN7Y3jVBozRBp6i_cEyGF1V-oqb4uHDEMHLNAstgt4HNXLJrwlGY6jJP6RChM0YVwLn43lW0fUr7amlnL5d9cERL7NZDDMuGDE56yLwRsPKAA2Ppn6bWZFC_FTt6L1m3M65zOdjhfWNLHhIzr5Rmg$tHfWag6QXz2PA26CFmRcbEelDU8FRml21bDC8IsL4D0"
 
@@ -62,7 +64,8 @@ except:
 jobs = getjobs(longurl)
 
 new_jobs = get_difference(old_jobs, jobs) # switch to old_jobs,jobs after test
-send_email(new_jobs)
+#send_email(new_jobs)
+send_email(jobs)
 print new_jobs
 
 json.dump(jobs,o)
